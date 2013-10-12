30caaeacf7f3b7ec5acfacf16f5c4b2fe6a44f3b
Jayson Cena <jayson.cena@clusterfoundry.com>
2013-10-12 17:33:06 +0000

    Implmeneted stage0(retrieving configs) and partial stage1(database importing)

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,9 @@
 from fabric.api import *
 from fabric.colors import *
 import cuisine
+import collections
+import json
+import re
 
 print green('Start', bold=True)
 env.passwords = {
@@ -17,6 +20,19 @@ env.roledefs = {
 }
 db_daemons_conf = {}
 db_shadow_conf = {}
+# {'198.50.141.68':
+#     ['b_d4t4s1', # app mysql user
+#     'mysql',     # database type
+#     'any',       # ?
+#     '/home/clients/databases/b_d4t4s1/mysql', # app db dir
+#     '127.0.0.1', # mysqld IP
+#     '4000',      # mysqld port
+#     '',
+#     '',
+#     'ZgUUrFBa2W']} # password
+
+# this is where we're gonna save the info of the devpanel node
+devpanel_node = {}
 
 
 @roles('devpanel_nodes')
@@ -46,7 +62,66 @@ def setup_web():
 def setup_db():
 	cuisine.run('uname -a')
 
+@roles('devpanel_nodes')
+def prepare_db():
+	mysql_opts = '--skip-column-names -NAB -u%s -h%s -P%s' % ('root', db_shadow_conf[env.host][4], db_shadow_conf[env.host][5])
+
+	devpanel_node[env.host] = { 'db': {} }
+
+	with hide('output'):
+		# Test MySQL access
+		if run('mysql %s -e \'SELECT "OK" FROM DUAL\' ; true' % mysql_opts).endswith('OK'):
+			# connected to mysql successfully,
+			# proceed with dumping users
+			mysql_query = "select concat(user,'|',host,'|',password) from mysql.user where user != '' and host != ''"
+			mysql_res = run('mysql %s -e "%s"' % (mysql_opts, mysql_query))
+			mysql_useraccounts = []
+			for line in mysql_res.split('\n'):
+				arr = line.strip().split('|')
+				mysql_query = "SHOW GRANTS FOR '%s'@'%s'" % (arr[0], arr[1])
+				with settings(warn_only=True):
+					mysql_res = run('mysql %s -e "%s"' % (mysql_opts, mysql_query))
+				if mysql_res.return_code != 0: continue
+				mysql_useraccounts.append({ 'user': arr[0], 'properties': {'host': arr[1], 'pass_hash': arr[2], 'grants': mysql_res.strip().split('\r\n') }})
+			# push users data to dictionary
+			devpanel_node[env.host]['db'].update(users = mysql_useraccounts)
+#			print json.dumps(devpanel_node, indent=2)
+
+		else:
+			print 'Error accessing database @ %s' % env.host
+			raise
+
+@roles('devpanel_nodes')
+def dump_schemas():
+	mysql_opts = '--skip-column-names -NAB -u%s -h%s -P%s' % ('root', db_shadow_conf[env.host][4], db_shadow_conf[env.host][5])
+
+	with hide('output'):
+		mysql_query = 'show databases'
+		# get list of databases
+		mysql_res = run('mysql %s -e "%s"' % (mysql_opts, mysql_query))
+		mysql_database = mysql_res.strip().split('\r\n')
+		for unused_db in [ 'information_schema', 'performance_schema', 'mysql', 'test' ]:
+			mysql_database.remove(unused_db)
+
+		print green('[%s]: Schema(s) to dump: %s' % (env.host, mysql_database))
+		for schema_to_dump in mysql_database:
+			run('mysqldump -u%s -h%s -P%s --databases %s > /tmp/%s.sql' % ('root', db_shadow_conf[env.host][4], db_shadow_conf[env.host][5], schema_to_dump, schema_to_dump))
+			print green('[%s]: Schema %s dumped to /tmp/%s.sql' % (env.host, schema_to_dump, schema_to_dump))
+
+
+
 if __name__ == '__main__':
 	execute(get_devpanel_config)
-	execute(setup_web)
-	execute(setup_db)
+	execute(prepare_db)
+	execute(dump_schemas)
+#	execute(setup_web)
+#	execute(setup_db)
+
+
+
+# split grants
+#				mysql_user_grant = []
+#				for line_grant in mysql_res.strip().split('\r\n'):
+#					line_split = re.split("GRANT (.*) ON (.*) TO '[^']+'@'[^']+'(( IDENTIFIED BY [A-Z]+ .*){0,1} WITH (.*)){0,1}", line_grant, flags=re.I)
+#					mysql_user_grant.append(line_split)
+
